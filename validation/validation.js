e6675314d1c12169f45c50b4ffb2a5ab0f19b7ca
Makuschenko Sergey <me@example.com>
2017-05-18 23:17:33 +0300

    added regexp

--- validation/validation.js
+++ validation/validation.js
@@ -1,1 +1,13 @@
 "use strict"
+
+var input = document.getElementById("login"),
+	re = /[a-z]/;
+
+input.oninput = function(){
+	// document.getElementById('log-msg').innerHTML = input.value;
+	if (re.test(input.value)){
+		document.getElementById('log-msg').innerHTML = "Good";
+	}else{
+		document.getElementById('log-msg').innerHTML = "bad";
+	}
+}
