c01db77259dc729dd5f3f3bd50fa9e05285273e2
Makuschenko Sergey <me@example.com>
2017-05-18 23:22:48 +0300

    syntax correction

--- validation/validation.js
+++ validation/validation.js
@@ -3,11 +3,11 @@
 var input = document.getElementById("login"),
 	re = /[a-z]/;
 
-input.oninput = function(){
+input.oninput = function() {
 	// document.getElementById('log-msg').innerHTML = input.value;
-	if (re.test(input.value)){
+	if (re.test(input.value)) {
 		document.getElementById('log-msg').innerHTML = "Good";
-	}else{
+	} else {
 		document.getElementById('log-msg').innerHTML = "bad";
 	}
 }
