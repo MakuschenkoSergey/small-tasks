eb7cce02e1750d7c4c2e278d053bf7c07e1734a8
Makuschenko Sergey <me@example.com>
2017-05-23 00:48:06 +0300

    some js changes

--- validation/validation.js
+++ validation/validation.js
@@ -1,13 +1,25 @@
 "use strict"
 
 var input = document.getElementById("login"),
-	re = /[a-z]/;
+	re = /\w+/;
+	pass = //;
 
 input.oninput = function() {
-	// document.getElementById('log-msg').innerHTML = input.value;
+	
 	if (re.test(input.value)) {
 		document.getElementById('log-msg').innerHTML = "Good";
+		document.getElementById('log-msg').style = "border: 1px solid lightgreen";		
 	} else {
 		document.getElementById('log-msg').innerHTML = "bad";
+		document.getElementById('log-msg').style = "border: 1px solid red";
 	}
 }
+	
+// усложнить регулярное выражение чтобы валидировались еще и цифры
+// изменить цвет бордера и текста всответствии результату.
+// добавить очистку сообщений при пустоте (возникает после бэкспейса)
+// в длиннуне не больше 10 ти символов
+
+// написать проверку пассворда, (применить # $ % хоть 1)
+
+// ** указать в сообщении необходимость спецсимволов
